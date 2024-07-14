20e2be206b85b7f7aaec5ae98eefc2226aa71f4b
Emmanuel <emmanuelreyes214@gmail.com>
2024-07-14 13:40:15 -0600

    cors update  1.1.1

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ import cookieParser from 'cookie-parser';
 import indexRoutes from './routers/user.routes.js'
 
 const app = express()
+app.use(cors({
+    origin: 'https://labarbada.store', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true  
+}))
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://amdin.labarbada.store ', 'https://labarbada.store/']
-
-app.use(cors({origin: 'https://labarbada.store', credentials:true}));
 app.use(express.json())
 app.use(cookieParser())
 
