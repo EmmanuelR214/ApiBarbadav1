dd72a7121ce04fb5b9228e35ff0bda1ec20c00ed
Emmanuel <emmanuelreyes214@gmail.com>
2024-07-22 15:34:46 -0600

    add publicidad 1.1.1

--- src/controllers/datauser.controllers.js
+++ src/controllers/datauser.controllers.js
@@ -1,24 +1,10 @@
-import { Coonexion } from "../db"
+import { Coonexion } from "../db.js"
 
-export const TraerDireccionUser = async(req, res) =>{
+export const TraerPublicidad = async(req, res) =>{
   try {
-      const id = req.params.idUser
-      const [[result]] = await Coonexion.execute('CALL ObtenerDirecciones(?)', [id])
-      const [result2] = await Coonexion.execute('SELECT ad.id_apodo, ad.apodo_direccion, i.url_icono FROM apodos_direcciones ad INNER JOIN iconos i ON ad.id_icono = i.id_icono')
-      res.status(200).json([result, result2])
+    const [[publicidad]] = await Coonexion.execute('CALL ObtenerPublicidad()')
+    res.status(200).json(publicidad)
   } catch (error) {
-      console.log(error)
-      res.status(500).json(['Error al traer direcciones'])
-  }
-}
-
-export const InsertarDireccion = async(req, res) => {
-  try {
-      const {direccion, descripcion, id_usuario, id_apodo } = req.body
-      await Coonexion.execute('CALL InsertarDireccion(?, ?, ?, ?)', [direccion, descripcion, id_usuario, id_apodo])
-      res.status(200).json(['Dirección agregada'])
-  } catch (error) {
-      console.log(error)
-      res.status(500).json(['Error al insertar la dirección'])
+    res.status(500).json(['Error al traer publicidad'])
   }
 }
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -2,6 +2,7 @@ import {Router} from 'express'
 import {validateSchema} from '../middlewares/validator.middleware.js'
 import { LoginSchema, RegisterSchema, SendMail, SearchPhoneSchema, RecoverPasswordSchema } from '../schemas/auth.schema.js'
 import { AlertUser, InsertarDireccion, LoginUser, RecoverPasswordEmail, RegisterFirebase, RegisterUser, SearchNumberPhoneRegister, sendEmail, TraerDireccionUser, verifYToken } from '../controllers/user.controllers.js'
+import { TraerPublicidad } from '../controllers/datauser.controllers.js'
 
 const router = Router()
 
@@ -25,6 +26,8 @@ router.get('/alert', AlertUser)
 router.get('/direcciones/:idUser', TraerDireccionUser)
 
 router.post('/insertarDireccion', InsertarDireccion)
+
+router.get('/publicidad', TraerPublicidad)
 
 
 export default router
